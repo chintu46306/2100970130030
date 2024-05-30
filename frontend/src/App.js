e2e32503d20c69fb74ec06fbb9efe625bf9fe8df
chintu46306 <crg46306@gmail.com>
2024-05-30 16:59:04 +0530

    added

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ProductList from './ProductList';
+import ProductDetails from './ProductDetails';
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<ProductList />} />
+                <Route path="/categories/:category/products/:productId" element={<ProductDetails />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
